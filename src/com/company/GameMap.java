bd34b69c5e90b6dacc5bed54e7010549137f6938
Anna <anna.wildt@hotmail.com>
2018-06-26 12:12:41 +0200

    CHange method name

--- src/com/company/GameMap.java
+++ src/com/company/GameMap.java
@@ -7,8 +7,8 @@ import java.nio.charset.Charset;
 
 public class GameMap {
     Terminal terminal;
-    private static final int WIDTH = 150;
-    private static final int HEIGHT = 100;
+    private static final int WIDTH = 100;
+    private static final int HEIGHT = 50;
 
     public void createGameMap() {
         terminal = TerminalFacade.createTerminal(System.in, System.out,
@@ -18,7 +18,7 @@ public class GameMap {
         int[] borderVertical = new int[]{0,WIDTH};
         int[] borderHorizontal = new int[]{0,HEIGHT};
 
-        terminal.applyBackgroundColor(Terminal.Color.RED);
+        terminal.applyBackgroundColor(Terminal.Color.CYAN);
 
         for(int v : borderVertical){
             for (int i = 0; i < HEIGHT; i++) {
