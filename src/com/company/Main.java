1b64526cf19c08e3c522e8dab86a88ab5c1b0cff
Patricio Calderon <patricio.calderon@outlook.com>
2018-06-26 10:38:44 +0200

    Added Lanterna

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,32 @@
 package com.company;
 
+import com.googlecode.lanterna.*;
+import com.googlecode.lanterna.input.Key;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.nio.charset.Charset;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws InterruptedExeption {
+
+        Terminal terminal = TerminalFacade.createTerminal(System.in,
+                            System.out, Charset.forName("UTF8"));
+
+        terminal.enterPrivateMode();
+
+        while(true) {
+
+            Key key;
+            do{
+                Thread.sleep(5 );
+                key =terminal.readInput();
+            }
+            while(key == null);
+
+            System.out.println(key.getCharacter()+ " " + key.getKind());
+        }
+
     }
+
 }
